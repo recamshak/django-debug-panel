cc536a30adf9898d7c227eeb0eff8fbffe86bd10
Joël Billaud <bibi@kis-billaud.epfl.ch>
2013-06-05 11:11:48 +0200

    added BSD license and pipy setup

---
+++ setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+setup(
+    name='django-debug-panel',
+    version='0.5.0',
+    author='Joël Billaud',
+    author_email='jbillaud@gmail.com',
+    packages=['debug_panel'],
+    url='https://github.com/recamshak/django-debug-panel',
+    license='BSD',
+    description='django-debug-toolbar in WebKit DevTools. Works fine with background Ajax requests and non-HTML responses',
+    long_description=open('README.rst').read(),
+    install_requires=[
+        "django-debug-toolbar >= 0.9.0"
+    ],
+)

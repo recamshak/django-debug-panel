885bd15269c0a547805731735f0808f2bfa8ac5b
Joël Billaud <bibi@kis-billaud.epfl.ch>
2013-06-21 10:11:15 +0200

    no need to add explicitly the app urls

--- debug_panel/__init__.py
+++ debug_panel/__init__.py
@@ -6,13 +6,26 @@ from django.shortcuts import render_to_response
 from django.views.static import serve
 import thread
 
-    
+import urls as debug_panel_urlconf
+
+debug_toolbar_process_request = DebugToolbarMiddleware.process_request
+
+
+def process_request(self, request):
+    try:
+        res = resolve(request.path, urlconf=debug_panel_urlconf)
+    except Resolver404:
+        return debug_toolbar_process_request(self, request)
+
+    return res.func(request, *res.args, **res.kwargs)
+
+
 def process_response(self, request, response):
     __traceback_hide__ = True
 
     ident = thread.get_ident()
     toolbar = self.__class__.debug_toolbars.get(ident)
-    
+
     try:
         current_view = resolve(request.path)[0]
     except Resolver404:
@@ -40,9 +53,10 @@ def process_response(self, request, response):
     stats = DebugData(data=toolbar.render_toolbar())
     stats.save()
     response['X-debug-data-url'] = request.build_absolute_uri(
-        reverse('debug_data', kwargs={'id': stats.id}))
+        reverse('debug_data', urlconf=debug_panel_urlconf, kwargs={'id': stats.id}))
 
     del self.__class__.debug_toolbars[ident]
     return response
 
 DebugToolbarMiddleware.process_response = process_response
+DebugToolbarMiddleware.process_request = process_request

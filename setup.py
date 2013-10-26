158dee88d12e9445ad10f9fb5e876294741c3efd
Joel Billaud <jbillaud@gmail.com>
2013-10-26 12:22:35 +0900

    new pypi release

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='django-debug-panel',
-    version='0.5.9',
+    version='0.6.0',
     author='Joël Billaud',
     author_email='jbillaud@gmail.com',
     packages=['debug_panel'],

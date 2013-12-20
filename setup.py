78843d624f6ebaaabd7c24f5e9b2de8255ffb8c1
Joel Billaud <jbillaud@gmail.com>
2013-12-20 19:50:04 +0900

    version bump

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='django-debug-panel',
-    version='0.7.1',
+    version='0.7.2',
     author=u'Joël Billaud',
     author_email='jbillaud@gmail.com',
     packages=['debug_panel'],
